a537e81cab4231b32d4f95bb802a2fe2e2af1329
melki123shi <melkishitesfayeangassa@gmail.com>
2025-08-05 13:55:26 +0300

    Fix Header Ui

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,10 @@
 const Header = () => {
   return (
-    <div className="text-center mb-8">
-      <h1 className="text-4xl font-bold text-gray-900 mb-4">
+    <div className="mb-8 mx-21 text-white pt-8">
+      <h1 className="text-7xl font-bold mb-4">
         Find Your Bonds
       </h1>
-      <p className="text-lg text-gray-600 max-w-2xl mx-auto">
+      <p className="text-lg max-w-2xl">
         Search and filter bonds by coupon rate and maturity date. Find the
         perfect investment opportunities that match your criteria.
       </p>
